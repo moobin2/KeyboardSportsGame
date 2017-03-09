2538878213eb143dd88228e87170c75e828ab045
moobin2 <anqls20000@naver.com>
2017-03-10 05:39:14 +0900

    매니저 추가(아직 미완)
    게임매니저, UI매니저 , 씬매니저, 매니저템플릿 추가
    아직 미완

---
+++ Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.gameObject.name = "[Manager]GamaManager";
+        DontDestroyOnLoad(this);
+
+        ManagerInit();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void ManagerInit()
+    {
+        UIManager.Create();
+        GameSceneManager.Create();
+    }
+}
