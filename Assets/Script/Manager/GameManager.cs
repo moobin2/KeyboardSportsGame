beb57c739a0de1ebbf326f71a729a567d1ec3820
moobin2 <anqls20000@naver.com>
2017-03-21 12:42:27 +0900

    매니저 및 템플릿등 추가

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -4,25 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
-	// Use this for initialization
 	void Start ()
     {
-        this.gameObject.name = "[Manager]GamaManager";
-        DontDestroyOnLoad(this);
+        this.gameObject.name = "[Manager]GamaManager";      // 해당 오브젝트이름 바꾸기
+        DontDestroyOnLoad(this);                            // 매니저이기때문에 계속 살려두어야한다.
 
         ManagerInit();
     }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
 
     void ManagerInit()
     {
-        UIManager.Create();
+        UIManager.Create();                                 // UI매니저, 씬매니저 생성
         GameSceneManager.Create();
     }
 }
---
+++ Assets/Script/UI/UIPanel_TitleScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPanel_TitleScene : UIPanel_Template<UIPanel_TitleScene>
+{
+    public void OnClickStart()
+    {
+        GameSceneManager.Instance.ChangeScene(EGameScene.MAIN);
+    }
+}
