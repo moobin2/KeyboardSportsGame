cdf2fe02a06d9e77054038c987ef50ed6b84cf77
오무빈 <anqls20000@naver.com>
2017-04-07 18:15:57 +0900

    이펙트매니저 완성 및 이펙트추가, 플레이어컨트롤러에 추가
    이펙트매니저 생성후 모든 이펙트 관리
    이펙트는 addEffect함수로 이펙트 추가하고 오브젝트풀로 관리.
    이동할때마다 해당위치 키보드이펙트 추가함

---
+++ Assets/Script/Character/Controller_Castle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Controller_Castle : MonoBehaviour
+{
+    private Animator    _anim;
+    private bool        _bIsOpen;
+    // Use this for initialization
+    void Start ()
+    {
+        _bIsOpen = false;
+        _anim = GetComponent<Animator>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
