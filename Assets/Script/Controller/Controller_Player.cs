954d678f803b0dcd27b63ff50be347c02f728c94
677283 <677283@naver.com>
2017-04-11 18:21:24 +0900

    키입력 수정.

--- Assets/Script/Controller/Controller_Player.cs
+++ Assets/Script/Controller/Controller_Player.cs
@@ -14,16 +14,22 @@ public class Controller_Player : MonoBehaviour
     private float _fDestTime;
     private GameObject _keyboard;
     private bool _bIsMoving = false;
+	private Dictionary<char, Vector3> _dicKeyPosition;
 
     // Use this for initialization
     void Start()
     {
-        _fsmAnim = GetComponent<FSM_Player>();
-        _keyboard = GameObject.Find("Keyboard_Button");
+		_dicKeyPosition = new Dictionary<char, Vector3>();
+		_fsmAnim = GetComponent<FSM_Player>();
+		Transform[] keyTrans = GameObject.Find("Keyboard_Button").GetComponentsInChildren<Transform>();
 
+		for (int i=1; i < keyTrans.Length; i++)
+		{
+			_dicKeyPosition.Add(keyTrans[i].name[0].ToString().ToLower()[0], keyTrans[i].position);
+		}
         _fsmAnim.SetState(UnitState.Idle);
 
-        Manager_Effect.Instance.AddEffect("DestPosition", "FX_DestPosition", 10);
+        //Manager_Effect.Instance.AddEffect("DestPosition", "FX_DestPosition", 10);
     }
 
     // Update is called once per frame
@@ -53,15 +59,14 @@ public class Controller_Player : MonoBehaviour
         _bIsMoving = false;
     }
 
-    void SetDestPosition(string KeyButtonName)
+    void SetDestPosition(char KeyButtonName)
     {
         _bIsMoving = true;
 
         // 가야될 위치벡터
-        string ButtonObjectName = KeyButtonName + "_button";
-        Vector3 destPostion = _keyboard.transform.FindChild(ButtonObjectName).transform.position;
+		Vector3 destPostion = _dicKeyPosition[KeyButtonName];
         destPostion.y = 0;
-        Manager_Effect.Instance.PlayEffect("DestPosition", destPostion);
+        //Manager_Effect.Instance.PlayEffect("DestPosition", destPostion);
 
         // 가야될 방향벡터 및 각도
         Vector3 nextDirection = (destPostion - transform.position).normalized;
@@ -82,179 +87,181 @@ public class Controller_Player : MonoBehaviour
 
     void KeyBoardInput()
     {
-        if (Input.anyKey)
+        if (Input.inputString != "")
         {
-            switch (Input.inputString)
-            {
-                // 키보드 상단 줄
-                case "Q":
-                case "q":
-                    SetDestPosition("Q");
-                    Debug.Log("Q button");
-                    break;
-                case "W":
-                case "w":
-                    SetDestPosition("W");
-                    Debug.Log("W button");
-                    break;
-                case "E":
-                case "e":
-                    SetDestPosition("E");
-                    Debug.Log("E button");
-                    break;
-                case "R":
-                case "r":
-                    SetDestPosition("R");
-                    Debug.Log("R button");
-                    break;
-                case "T":
-                case "t":
-                    SetDestPosition("T");
-                    Debug.Log("T button");
-                    break;
-                case "Y":
-                case "y":
-                    SetDestPosition("Y");
-                    Debug.Log("Y button");
-                    break;
-                case "U":
-                case "u":
-                    SetDestPosition("U");
-                    Debug.Log("U button");
-                    break;
-                case "I":
-                case "i":
-                    SetDestPosition("I");
-                    Debug.Log("I button");
-                    break;
-                case "O":
-                case "o":
-                    SetDestPosition("O");
-                    Debug.Log("O button");
-                    break;
-                case "P":
-                case "p":
-                    SetDestPosition("P");
-                    Debug.Log("P button");
-                    break;
-                case "[":
-                    SetDestPosition("[");
-                    Debug.Log("[ button");
-                    break;
-                case "]":
-                    SetDestPosition("]");
-                    Debug.Log("] button");
-                    break;
-                //키보드 중간줄
-                case "A":
-                case "a":
-                    SetDestPosition("A");
-                    Debug.Log("A button");
-                    break;
-                case "S":
-                case "s":
-                    SetDestPosition("S");
-                    Debug.Log("S button");
-                    break;
-                case "D":
-                case "d":
-                    SetDestPosition("D");
-                    Debug.Log("D button");
-                    break;
-                case "F":
-                case "f":
-                    SetDestPosition("F");
-                    Debug.Log("F button");
-                    break;
-                case "G":
-                case "g":
-                    SetDestPosition("G");
-                    Debug.Log("G button");
-                    break;
-                case "H":
-                case "h":
-                    SetDestPosition("H");
-                    Debug.Log("H button");
-                    break;
-                case "J":
-                case "j":
-                    SetDestPosition("J");
-                    Debug.Log("J button");
-                    break;
-                case "K":
-                case "k":
-                    SetDestPosition("K");
-                    Debug.Log("K button");
-                    break;
-                case "L":
-                case "l":
-                    SetDestPosition("L");
-                    Debug.Log("L button");
-                    break;
-                case ";":
-                    SetDestPosition(";");
-                    Debug.Log("; button");
-                    break;
-                case "'":
-                    SetDestPosition("'");
-                    Debug.Log("' button");
-                    break;
-                //키보드 하단줄
-                case "Z":
-                case "z":
-                    SetDestPosition("Z");
-                    Debug.Log("Z button");
-                    break;
-                case "X":
-                case "x":
-                    SetDestPosition("X");
-                    Debug.Log("X button");
-                    break;
-                case "C":
-                case "c":
-                    SetDestPosition("C");
-                    Debug.Log("C button");
-                    break;
-                case "V":
-                case "v":
-                    SetDestPosition("V");
-                    Debug.Log("V button");
-                    break;
-                case "B":
-                case "b":
-                    SetDestPosition("B");
-                    Debug.Log("b button");
-                    break;
-                case "N":
-                case "n":
-                    SetDestPosition("N");
-                    Debug.Log("N button");
-                    break;
-                case "M":
-                case "m":
-                    SetDestPosition("M");
-                    Debug.Log("M button");
-                    break;
-                case ",":
-                    SetDestPosition(",");
-                    Debug.Log(", button");
-                    break;
-                case ".":
-                    SetDestPosition(".");
-                    Debug.Log(". button");
-                    break;
-                case "/":
-                    SetDestPosition("/");
-                    Debug.Log("/ button");
-                    break;
-                case " ":
-                    Debug.Log("Space Button");
-                    SetAttack();
-                    break;
-            }
-
-        }
-    }
+			SetDestPosition(Input.inputString[0]);
+			#region 수정전
+			//switch (Input.inputString)
+			//{
+			//    // 키보드 상단 줄
+			//    case "Q":
+			//    case "q":
+			//        SetDestPosition("Q");
+			//        Debug.Log("Q button");
+			//        break;
+			//    case "W":
+			//    case "w":
+			//        SetDestPosition("W");
+			//        Debug.Log("W button");
+			//        break;
+			//    case "E":
+			//    case "e":
+			//        SetDestPosition("E");
+			//        Debug.Log("E button");
+			//        break;
+			//    case "R":
+			//    case "r":
+			//        SetDestPosition("R");
+			//        Debug.Log("R button");
+			//        break;
+			//    case "T":
+			//    case "t":
+			//        SetDestPosition("T");
+			//        Debug.Log("T button");
+			//        break;
+			//    case "Y":
+			//    case "y":
+			//        SetDestPosition("Y");
+			//        Debug.Log("Y button");
+			//        break;
+			//    case "U":
+			//    case "u":
+			//        SetDestPosition("U");
+			//        Debug.Log("U button");
+			//        break;
+			//    case "I":
+			//    case "i":
+			//        SetDestPosition("I");
+			//        Debug.Log("I button");
+			//        break;
+			//    case "O":
+			//    case "o":
+			//        SetDestPosition("O");
+			//        Debug.Log("O button");
+			//        break;
+			//    case "P":
+			//    case "p":
+			//        SetDestPosition("P");
+			//        Debug.Log("P button");
+			//        break;
+			//    case "[":
+			//        SetDestPosition("[");
+			//        Debug.Log("[ button");
+			//        break;
+			//    case "]":
+			//        SetDestPosition("]");
+			//        Debug.Log("] button");
+			//        break;
+			//    //키보드 중간줄
+			//    case "A":
+			//    case "a":
+			//        SetDestPosition("A");
+			//        Debug.Log("A button");
+			//        break;
+			//    case "S":
+			//    case "s":
+			//        SetDestPosition("S");
+			//        Debug.Log("S button");
+			//        break;
+			//    case "D":
+			//    case "d":
+			//        SetDestPosition("D");
+			//        Debug.Log("D button");
+			//        break;
+			//    case "F":
+			//    case "f":
+			//        SetDestPosition("F");
+			//        Debug.Log("F button");
+			//        break;
+			//    case "G":
+			//    case "g":
+			//        SetDestPosition("G");
+			//        Debug.Log("G button");
+			//        break;
+			//    case "H":
+			//    case "h":
+			//        SetDestPosition("H");
+			//        Debug.Log("H button");
+			//        break;
+			//    case "J":
+			//    case "j":
+			//        SetDestPosition("J");
+			//        Debug.Log("J button");
+			//        break;
+			//    case "K":
+			//    case "k":
+			//        SetDestPosition("K");
+			//        Debug.Log("K button");
+			//        break;
+			//    case "L":
+			//    case "l":
+			//        SetDestPosition("L");
+			//        Debug.Log("L button");
+			//        break;
+			//    case ";":
+			//        SetDestPosition(";");
+			//        Debug.Log("; button");
+			//        break;
+			//    case "'":
+			//        SetDestPosition("'");
+			//        Debug.Log("' button");
+			//        break;
+			//    //키보드 하단줄
+			//    case "Z":
+			//    case "z":
+			//        SetDestPosition("Z");
+			//        Debug.Log("Z button");
+			//        break;
+			//    case "X":
+			//    case "x":
+			//        SetDestPosition("X");
+			//        Debug.Log("X button");
+			//        break;
+			//    case "C":
+			//    case "c":
+			//        SetDestPosition("C");
+			//        Debug.Log("C button");
+			//        break;
+			//    case "V":
+			//    case "v":
+			//        SetDestPosition("V");
+			//        Debug.Log("V button");
+			//        break;
+			//    case "B":
+			//    case "b":
+			//        SetDestPosition("B");
+			//        Debug.Log("b button");
+			//        break;
+			//    case "N":
+			//    case "n":
+			//        SetDestPosition("N");
+			//        Debug.Log("N button");
+			//        break;
+			//    case "M":
+			//    case "m":
+			//        SetDestPosition("M");
+			//        Debug.Log("M button");
+			//        break;
+			//    case ",":
+			//        SetDestPosition(",");
+			//        Debug.Log(", button");
+			//        break;
+			//    case ".":
+			//        SetDestPosition(".");
+			//        Debug.Log(". button");
+			//        break;
+			//    case "/":
+			//        SetDestPosition("/");
+			//        Debug.Log("/ button");
+			//        break;
+			//    case " ":
+			//        Debug.Log("Space Button");
+			//        SetAttack();
+			//        break;
+			//}
+			#endregion
+		}
+	}
 
     void SetAttack()    // damaged 모션 추가해야함.
     {
