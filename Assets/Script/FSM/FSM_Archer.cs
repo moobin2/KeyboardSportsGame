5efdf7323e65f7cd8bccfde4a9a50a33f0e7cc02
moobin2 <anqls20000@naver.com>
2017-04-11 13:45:04 +0900

    archer, warrior추가 및 player회전오류 수정

---
+++ Assets/Script/FSM/FSM_Archer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM_Archer : FSM_Base
+{
+    IEnumerator FireArrow()
+    {
+        Debug.Log("Enter Attack1_State");
+        float motionTime = 0.0f;
+        _anim.CrossFade("Melee Right Attack 01", crossFadeTime);
+        yield return null;
+
+        while (currentState == UnitState.Attack1)
+        {
+            motionTime += Time.deltaTime;
+            if (motionTime > _animList["Melee Right Attack 01"])
+            {
+                SetState(UnitState.Idle);
+            }
+            yield return null;
+        }
+    }
+}
