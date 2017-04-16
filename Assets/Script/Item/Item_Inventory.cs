0514a13667975934bd58b8048260d3cbd56e122f
677283 <677283@naver.com>
2017-04-17 01:01:31 +0900

    인벤토리 UI 만드는중.

---
+++ Assets/Script/Item/Item_Inventory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum ITEMTYPE
+{
+	HAIR = 0,
+	SKIN,
+	WING,
+	WEAPON,
+
+	MAXITEM
+}
+
+[Serializable]
+public class InventoryData
+{
+	public List<string> _liftHairList;
+	public List<string> _liftSkinList;
+	public List<string> _liftWingList;
+	public List<string> _liftWeaponList;
+	public List<string> _currentItem;
+}
+
+public class Item_Inventory : MonoBehaviour
+{
+	private InventoryData _data;
+	public InventoryData Data
+	{
+		get
+		{
+			return _data;
+		}
+	}
+	private void Awake()
+	{
+		TextAsset jsonText = (TextAsset)Resources.Load("JsonFiles/Inventory");
+		_data = JsonUtility.FromJson<InventoryData>(jsonText.text);
+
+	}
+}
