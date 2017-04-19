1c9922180b6c4127ef5d8c5fe61b983ebdb17390
677283 <677283@naver.com>
2017-04-19 14:07:18 +0900

    커밋용

--- Assets/Script/UI/Inventory_List.cs
+++ Assets/Script/UI/Inventory_List.cs
@@ -4,29 +4,45 @@ using UnityEngine;
 
 public class Inventory_List : MonoBehaviour
 {
+	static private Camera _viewCam;
 	private GameObject _player;
 	private List<string> _playerInven;
 	private int _itemCount;
 	private List<string> _itemList;
-	private List<GameObject> _viewObject;
+	private List<GameObject> _viewObjectList;
+	private List<RenderTexture> _viewTextureList;
 	private List<GameObject> _slotList;
+	private GameObject _slotBase;
+	private GameObject _viewObjectZone;
 
 	private void Awake()
 	{
+		if (_viewCam == null)
+		{
+			GameObject gObj = new GameObject("ItemViewCam");
+			_viewCam = gObj.AddComponent<Camera>();
+			_viewCam.cullingMask = LayerMask.NameToLayer("Item");
+		}
+			
 		Item_Inventory _inventory;
 		_inventory = _player.GetComponent<Item_Inventory>();
 		switch (this.gameObject.name)
 		{
 			case "Hair":
 				_playerInven = _inventory.Data._liftHairList;
+				_viewObjectZone = new GameObject("HairZone");
 				break;
 			case "Skin":
 				_playerInven = _inventory.Data._liftSkinList;
+				_viewObjectZone = new GameObject("SkinZone");
 				break;
 			case "Wing":
 				_playerInven = _inventory.Data._liftWingList;
+				_viewObjectZone = new GameObject("WingZone");
 				break;
 		}
+		_viewObjectZone.transform.position = new Vector3(2000, 2000, 2000);
+		
 		Refresh();
 	}
 	private void Refresh()
@@ -40,9 +56,22 @@ public class Inventory_List : MonoBehaviour
 				{
 					_itemList.Add(_playerInven[i]);
 					/*
-					뷰오브젝트 추가.
 					슬롯 추가. 
 					 */
+					GameObject gObj = Instantiate(_slotBase);
+					gObj.transform.SetParent(this.transform);
+					gObj.transform.localPosition = Vector3.zero;
+					_slotList.Add(gObj);
+					/*
+					뷰오브젝트 추가.
+					*/
+					GameObject createItem = Manager_Item.Instance.CreateItem(_playerInven[i]);
+					createItem.transform.SetParent(_viewObjectZone.transform);
+					createItem.transform.localPosition = Vector3.zero;
+					_viewObjectList.Add(createItem);
+					_viewTextureList.Add(new RenderTexture(300, 300, 0));
+					_viewCam.targetTexture = _viewTextureList[i];
+					_viewCam.Render();
 
 					gap--;
 					if (gap <= 0)
