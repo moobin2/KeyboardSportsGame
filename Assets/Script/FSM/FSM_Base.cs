b8a394802ad7c3ea011c32a33753a9e2cb2c65d0
moobin2 <anqls20000@naver.com>
2017-04-19 14:36:57 +0900

    텍스쳐와 폰트, 아틀라스추가 및 애니메이션 메카님으로 교체
    케릭터 인잇을 어디서 해야할지 상의하교 작성해야함.

--- Assets/Script/FSM/FSM_Base.cs
+++ Assets/Script/FSM/FSM_Base.cs
@@ -2,47 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum UnitState
+public enum BASESTATE
 {
-    Idle, Run, Dameged, Die,                                // 공통모션
-    Attack1, Attack2, Attack3, JumpAttack, SpinAttack,      // 플레이어
-    ArrowAim, ArrowShoot,                                   // 궁수
-    TH_Idle, TH_Run, TH_Attack1, TH_Attack2,
-
+    Normal, TwoHand, Flying
 }
 
-[RequireComponent(typeof(Animation))]
+public enum MOTIONSTATE
+{
+    Idle, 
+    Run, 
+    Attack1, 
+    Attack2, 
+    Attack3, 
+    JumpAttack, 
+    SpinAttack,	
+    CrossbowAim, 
+    CrossbowShoot, 
+    Damaged, 
+    Die		
+}
+
+[RequireComponent(typeof(Animator))]
 public class FSM_Base : MonoBehaviour
 {
-    public      UnitState currentState;
-    public      float crossFadeTime = 0.2f;
-    protected   Animation _anim;
-    protected   Dictionary<string, float> _animList;
+    public BASESTATE currentBaseState;
+    public BASESTATE CurrentBaseState { get { return currentBaseState; } }
+    public MOTIONSTATE currentMotionState;
+    public MOTIONSTATE CurrentMotionState { get { return currentMotionState; } }
+
+    //public      float crossFadeTime = 0.2f;
+    //protected   Dictionary<string, float> _animList;
+    protected   Animator _anim;
     protected   bool _isChangeState;
 
     // Use this for initialization
 
-    protected void Start()
+    void Start()
     {
-        //Debug.Log("Base Start");
-        _anim = GetComponent<Animation>();
+        _anim = GetComponent<Animator>();
         _isChangeState = false;
-
-        _animList = new Dictionary<string, float>();
-
-        foreach (AnimationState AnimState in _anim)
-        {
-            _animList.Add(AnimState.clip.name, AnimState.clip.length);
-            //Debug.Log(AnimState.clip.name.ToString() + ": length(" + AnimState.clip.length.ToString() + ")");
-        }
 
         StartCoroutine("FsmMain");
     }
 
-    public void SetState(UnitState state)
+    public void SetState(BASESTATE baseState, MOTIONSTATE motionState)
     {
         //Debug.Log("Chage to " + state.ToString());
-        currentState = state;
+        currentBaseState = baseState;
+        currentMotionState = motionState;
         _isChangeState = true;
     }
 
@@ -53,9 +60,9 @@ public class FSM_Base : MonoBehaviour
             if (_isChangeState == true)
             {
                 _isChangeState = false;
-                StartCoroutine(currentState.ToString());
-                Debug.Log(currentState.ToString());
-                yield return null; 
+                _anim.SetInteger("BaseState", (int)currentBaseState);
+                _anim.SetInteger("MotionState", (int)currentMotionState);
+                yield return null;
             }
             else
             {
@@ -64,135 +71,135 @@ public class FSM_Base : MonoBehaviour
         }
     }
 
-    protected IEnumerator Idle()
-    {
-        //Debug.Log("Enter IdleState");
-        _anim.CrossFade("Idle", crossFadeTime);
-        yield return null;
+    //protected IEnumerator Idle()
+    //{
+    //    //Debug.Log("Enter IdleState");
+    //    _anim.CrossFade("Idle", crossFadeTime);
+    //    yield return null;
 
-        while (currentState == UnitState.Idle)
-        {
-            yield return null;
-        }
-        //Debug.Log("Exit IdleState");
-    }
+    //    while (currentState == UnitState.Idle)
+    //    {
+    //        yield return null;
+    //    }
+    //    //Debug.Log("Exit IdleState");
+    //}
 
-    protected IEnumerator TH_Idle()
-    {
-        _anim.CrossFade("TH Sword Idle", crossFadeTime);
-        yield return null;
+    //protected IEnumerator TH_Idle()
+    //{
+    //    _anim.CrossFade("TH Sword Idle", crossFadeTime);
+    //    yield return null;
 
-        while (currentState == UnitState.TH_Idle)
-        {
-            yield return null;
-        }
-    }
+    //    while (currentState == UnitState.TH_Idle)
+    //    {
+    //        yield return null;
+    //    }
+    //}
 
-    protected IEnumerator Run()
-    {
-        //Debug.Log("Enter RunState");
-        _anim.CrossFade("Run", crossFadeTime);
-        yield return null;
+    //protected IEnumerator Run()
+    //{
+    //    //Debug.Log("Enter RunState");
+    //    _anim.CrossFade("Run", crossFadeTime);
+    //    yield return null;
 
-        while (currentState == UnitState.Run)
-        {
-            yield return null;
-        }
-        //Debug.Log("Exit RunState");
-    }
+    //    while (currentState == UnitState.Run)
+    //    {
+    //        yield return null;
+    //    }
+    //    //Debug.Log("Exit RunState");
+    //}
 
-    protected IEnumerator TH_Run()
-    {
-        //Debug.Log("Enter RunState");
-        _anim.CrossFade("TH Sword Run Without Root Motion", crossFadeTime);
-        yield return null;
+    //protected IEnumerator TH_Run()
+    //{
+    //    //Debug.Log("Enter RunState");
+    //    _anim.CrossFade("TH Sword Run Without Root Motion", crossFadeTime);
+    //    yield return null;
 
-        while (currentState == UnitState.TH_Run)
-        {
-            yield return null;
-        }
-        //Debug.Log("Exit RunState");
-    }
+    //    while (currentState == UnitState.TH_Run)
+    //    {
+    //        yield return null;
+    //    }
+    //    //Debug.Log("Exit RunState");
+    //}
 
-    protected IEnumerator Attack2()
-    {
-        //Debug.Log("Enter Attack2_State");
-        float motionTime = 0.0f;
-        _anim.CrossFade("Melee Right Attack 02", crossFadeTime);
-        yield return null;
+    //protected IEnumerator Attack2()
+    //{
+    //    //Debug.Log("Enter Attack2_State");
+    //    float motionTime = 0.0f;
+    //    _anim.CrossFade("Melee Right Attack 02", crossFadeTime);
+    //    yield return null;
 
-        while (currentState == UnitState.Attack2)
-        {
-            motionTime += Time.deltaTime;
-            if (motionTime > _animList["Melee Right Attack 02"])
-            {
-                SetState(UnitState.Idle);
-            }
-            yield return null;
-        }
-    }
+    //    while (currentState == UnitState.Attack2)
+    //    {
+    //        motionTime += Time.deltaTime;
+    //        if (motionTime > _animList["Melee Right Attack 02"])
+    //        {
+    //            SetState(UnitState.Idle);
+    //        }
+    //        yield return null;
+    //    }
+    //}
 
-    protected IEnumerator Attack3()
-    {
-        //Debug.Log("Enter Attack3_State");
-        float motionTime = 0.0f;
-        _anim.CrossFade("Melee Right Attack 03", crossFadeTime);
-        yield return null;
+    //protected IEnumerator Attack3()
+    //{
+    //    //Debug.Log("Enter Attack3_State");
+    //    float motionTime = 0.0f;
+    //    _anim.CrossFade("Melee Right Attack 03", crossFadeTime);
+    //    yield return null;
 
-        while (currentState == UnitState.Attack3)
-        {
-            motionTime += Time.deltaTime;
-            if (motionTime > _animList["Melee Right Attack 03"])
-            {
-                SetState(UnitState.Idle);
-            }
-            yield return null;
-        }
-    }
+    //    while (currentState == UnitState.Attack3)
+    //    {
+    //        motionTime += Time.deltaTime;
+    //        if (motionTime > _animList["Melee Right Attack 03"])
+    //        {
+    //            SetState(UnitState.Idle);
+    //        }
+    //        yield return null;
+    //    }
+    //}
 
-    protected IEnumerator TH_Attack1()
-    {
-        //Debug.Log("Enter Attack2_State");
-        float motionTime = 0.0f;
-        _anim.CrossFade("TH Sword Melee Attack 01", crossFadeTime);
-        yield return null;
+    //protected IEnumerator TH_Attack1()
+    //{
+    //    //Debug.Log("Enter Attack2_State");
+    //    float motionTime = 0.0f;
+    //    _anim.CrossFade("TH Sword Melee Attack 01", crossFadeTime);
+    //    yield return null;
 
-        while (currentState == UnitState.TH_Attack1)
-        {
-            motionTime += Time.deltaTime;
-            if (motionTime > _animList["TH Sword Melee Attack 01"])
-            {
-                SetState(UnitState.TH_Idle);
-            }
-            yield return null;
-        }
-    }
+    //    while (currentState == UnitState.TH_Attack1)
+    //    {
+    //        motionTime += Time.deltaTime;
+    //        if (motionTime > _animList["TH Sword Melee Attack 01"])
+    //        {
+    //            SetState(UnitState.TH_Idle);
+    //        }
+    //        yield return null;
+    //    }
+    //}
 
-    protected IEnumerator TH_Attack2()
-    {
-        //Debug.Log("Enter Attack3_State");
-        float motionTime = 0.0f;
-        _anim.CrossFade("TH Sword Melee Attack 02", crossFadeTime);
-        yield return null;
+    //protected IEnumerator TH_Attack2()
+    //{
+    //    //Debug.Log("Enter Attack3_State");
+    //    float motionTime = 0.0f;
+    //    _anim.CrossFade("TH Sword Melee Attack 02", crossFadeTime);
+    //    yield return null;
 
-        while (currentState == UnitState.TH_Attack2)
-        {
-            motionTime += Time.deltaTime;
-            if (motionTime > _animList["TH Sword Melee Attack 02"])
-            {
-                SetState(UnitState.TH_Idle);
-            }
-            yield return null;
-        }
-    }
+    //    while (currentState == UnitState.TH_Attack2)
+    //    {
+    //        motionTime += Time.deltaTime;
+    //        if (motionTime > _animList["TH Sword Melee Attack 02"])
+    //        {
+    //            SetState(UnitState.TH_Idle);
+    //        }
+    //        yield return null;
+    //    }
+    //}
 
-    protected IEnumerator Damaged()
-    {
-        yield return null;
-    }
+    //protected IEnumerator Damaged()
+    //{
+    //    yield return null;
+    //}
 
-    protected IEnumerator Die()
-    {
-        yield return null;
-    }
+    //protected IEnumerator Die()
+    //{
+    //    yield return null;
+    //}
 }
--- Assets/Script/FSM/FSM_Warrior.cs
+++ Assets/Script/FSM/FSM_Warrior.cs
@@ -4,39 +4,39 @@ using UnityEngine;
 
 public class FSM_Warrior : FSM_Base
 {
-    IEnumerator Attack()
-    {
-        Debug.Log("Enter Attack2_State");
-        float motionTime = 0.0f;
-        _anim.CrossFade("Melee Right Attack 02", crossFadeTime);
-        yield return null;
+    //IEnumerator Attack()
+    //{
+    //    Debug.Log("Enter Attack2_State");
+    //    float motionTime = 0.0f;
+    //    _anim.CrossFade("Melee Right Attack 02", crossFadeTime);
+    //    yield return null;
 
-        while (currentState == UnitState.Attack2)
-        {
-            motionTime += Time.deltaTime;
-            if (motionTime > _animList["Melee Right Attack 02"])
-            {
-                SetState(UnitState.Idle);
-            }
-            yield return null;
-        }
-    }
+    //    while (currentState == UnitState.Attack2)
+    //    {
+    //        motionTime += Time.deltaTime;
+    //        if (motionTime > _animList["Melee Right Attack 02"])
+    //        {
+    //            SetState(UnitState.Idle);
+    //        }
+    //        yield return null;
+    //    }
+    //}
 
-    IEnumerator Attack2()
-    {
-        Debug.Log("Enter Attack3_State");
-        float motionTime = 0.0f;
-        _anim.CrossFade("Melee Right Attack 03", crossFadeTime);
-        yield return null;
+    //IEnumerator Attack2()
+    //{
+    //    Debug.Log("Enter Attack3_State");
+    //    float motionTime = 0.0f;
+    //    _anim.CrossFade("Melee Right Attack 03", crossFadeTime);
+    //    yield return null;
 
-        while (currentState == UnitState.Attack3)
-        {
-            motionTime += Time.deltaTime;
-            if (motionTime > _animList["Melee Right Attack 03"])
-            {
-                SetState(UnitState.Idle);
-            }
-            yield return null;
-        }
-    }
+    //    while (currentState == UnitState.Attack3)
+    //    {
+    //        motionTime += Time.deltaTime;
+    //        if (motionTime > _animList["Melee Right Attack 03"])
+    //        {
+    //            SetState(UnitState.Idle);
+    //        }
+    //        yield return null;
+    //    }
+    //}
 }
